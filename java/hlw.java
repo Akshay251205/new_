137230ca082c3f362982077734405e37647b7fb6
Akshay251205 <akshaymjha@gmail.com>
2026-04-29 10:17:28 +0530

    Delete java directory

--- java/hlw.java
+++
@@ -1,9 +0,0 @@
-class hlw{
-    // hlw(){
-
-    // }
-    public static void main(String[] args) {
-        System.out.println("Hellwww");
-        System.out.print("Hiiiii");
-    }
-}
--- java/stringBuilderfun.java
+++
@@ -1,5 +0,0 @@
-public class stringBuilderfun {
-    public static void main(String args[]){
-        
-    }
-}
